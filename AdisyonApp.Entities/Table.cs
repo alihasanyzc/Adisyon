1c2c6f3687d804133a34564e3dc72ac7b23c1f98
Ali Hasan <alihasanyazici28@gmail.com>
2025-04-28 15:14:42 +0300

    onarıldı

--- AdisyonApp.Entities/Table.cs
+++ AdisyonApp.Entities/Table.cs
@@ -4,10 +4,10 @@ namespace AdisyonApp.Entities
     {
         // Properties
         public int Id { get; set; }
-        public string? TableName { get; set; }
+        public string TableName { get; set; }
         public List<Order>? Orders { get; set; }
         public bool IsOccupied { get; set; } = false; // Set true when order is plac
-        public Menu? Menu { get; set; }
+        
 
         public double TableTotalPrice { get; set; }
         
